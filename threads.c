a831d09349ac893ce1547129acde38fc24daa38e
saraivajv <joaovictors2010@gmail.com>
2023-09-17 22:00:11 -0300

    Computando tempo com time.h

--- threads.c
+++ threads.c
@@ -102,8 +102,7 @@ int main(int argc, char **argv)
     }
 
     //Criando as threads
-    struct rusage start_usage, end_usage;
-    struct timeval start_time, end_time;
+    time_t start, end;
     int p = atoi(argv[3]);
     printf("p = %d\n", p);
     int nThreads = lin[0] * col[1] / p;
@@ -128,20 +127,12 @@ int main(int argc, char **argv)
         }
         printf("p = %d\n", argumentos->p);
         pthread_create(&threads[i], NULL, multMatrix, argumentos);
-
-        getrusage(RUSAGE_THREAD, &start_usage); // pega o tempo de início do uso da CPU
-        gettimeofday(&start_time, NULL); // pega o tempo de início do uso da CPU
-
+        time(&start);
         pthread_join(threads[i], NULL);
-
-        gettimeofday(&end_time, NULL); // pega o tempo de uso da CPU
-        getrusage(RUSAGE_THREAD, &end_usage); // pega o tempo de uso da CPU
-        //calculando a diferença entre início e final de uso
-        long long start_microseconds = start_time.tv_sec * 1000000 + start_time.tv_usec;
-        long long end_microseconds = end_time.tv_sec * 1000000 + end_time.tv_usec;
-        long long cpu_time_microseconds = (end_usage.ru_utime.tv_sec * 1000000 + end_usage.ru_utime.tv_usec) - (start_usage.ru_utime.tv_sec * 1000000 + start_usage.ru_utime.tv_usec);
-        argumentos->inicioC += argumentos->p;
-        printf("CPU time for the thread: %lld microseconds\n", cpu_time_microseconds); // printando o tempo de uso
+        time(&end);
+        int cputime = end - start;
+        printf("Tempo de CPU usado pela thread: %d\n", cputime);
+        
     }
     free(matrizes[0]);
     free(matrizes[1]);
@@ -174,6 +165,11 @@ void* multMatrix(void* arg)
         printf("%d ", soma);
     }
     printf("\n");
+    long long start_microseconds = start_time.tv_sec * 1000000 + start_time.tv_usec;
+        long long end_microseconds = end_time.tv_sec * 1000000 + end_time.tv_usec;
+        long long cpu_time_microseconds = (end_usage.ru_utime.tv_sec * 1000000 + end_usage.ru_utime.tv_usec) - (start_usage.ru_utime.tv_sec * 1000000 + start_usage.ru_utime.tv_usec);
+        argumentos->inicioC += argumentos->p;
+        printf("CPU time for the thread: %lld microseconds\n", cpu_time_microseconds); // printando o tempo de uso
     printf("Cálculos concluídos!\n");
     pthread_exit(NULL);
 }
